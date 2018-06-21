bddb2b7d50b48ee6ee67d1e5356fcf995e84ffef
David Recanati <davidd.r.90@gmail.com>
2018-06-21 08:58:22 +0300

    cange play

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,54 +23,64 @@ export class HomePage {
     private file: File,
     public platform: Platform) { }
 
-    getAudioList() {
-      if(localStorage.getItem("audiolist")) {
-        this.audioList = JSON.parse(localStorage.getItem("audiolist"));
-        console.log(this.audioList);
-      }
+  getAudioList() {
+    if (localStorage.getItem("audiolist")) {
+      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+      console.log(this.audioList);
+    }
+  }
+
+  ionViewWillEnter() {
+    this.getAudioList();
+  }
+
+  startRecord() {
+    if (this.platform.is('ios')) {
+      //mckdlcmd
+      this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.m4a';
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.3gp';
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    }
+    this.audio.startRecord();
+    this.recording = true;
+  }
+
+  stopRecord() {
+    this.audio.stopRecord();
+    let data = { filename: this.fileName };
+    this.audioList.push(data);
+    localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+    this.recording = false;
+    this.getAudioList();
+  }
+
+  url = "https://firebasestorage.googleapis.com/v0/b/lets-talk-b433e.appspot.com/o/davidd.r.90%40gmail.com%2Faudio%2Frecord55201813655.mp3?alt=media&token=bcef5e7e-c9cb-4b1f-a71f-f58b354afc36";
+  playAudio(file, idx) {
+
+    try {
+      this.audio = this.media.create(this.url);
+      this.audio.play()
+    } catch (err) {
+      console.log(err);
     }
 
-    ionViewWillEnter() {
-      this.getAudioList();
+    /*
+    if (this.platform.is('ios')) {
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create(this.filePath);
     }
+    this.audio.play();
+    this.audio.setVolume(0.8);
+    */
+  }
 
-    startRecord() {
-      if (this.platform.is('ios')) {
-        //mckdlcmd
-        this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.m4a';
-        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
-        this.audio = this.media.create(this.filePath);
-      } else if (this.platform.is('android')) {
-        this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
-        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
-        this.audio = this.media.create(this.filePath);
-      }
-      this.audio.startRecord();
-      this.recording = true;
-    }
 
-    stopRecord() {
-      this.audio.stopRecord();
-      let data = { filename: this.fileName };
-      this.audioList.push(data);
-      localStorage.setItem("audiolist", JSON.stringify(this.audioList));
-      this.recording = false;
-      this.getAudioList();
-    }
-
-    
-    playAudio(file,idx) {
-      if (this.platform.is('ios')) {
-        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
-        this.audio = this.media.create(this.filePath);
-      } else if (this.platform.is('android')) {
-        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
-        this.audio = this.media.create(this.filePath);
-      }
-      this.audio.play();
-      this.audio.setVolume(0.8);
-    }
-
-    
 
 }
